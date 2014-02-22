b0ea3c4a6e020f615bba0d877536a03850cf8fb0
PEMapModder <pemapmodder1970@gmail.com>
2014-02-22 12:30:40 +0800

    generator starting

--- src/droidPluginGen/pluginGen/generator/Generator.java
+++ src/droidPluginGen/pluginGen/generator/Generator.java
@@ -1,10 +1,14 @@
 package droidPluginGen.pluginGen.generator;
 
+import java.io.*;
+
 public class Generator {
 public Generator(){
 
 }
 @Override public String toString(){
 
+}
+public void outWrite(OutputStreamWriter osw)throws IOException{
 }
 }
