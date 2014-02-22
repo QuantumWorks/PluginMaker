50a0a766bd66c718558947760b8f2f569c59e7c5
PEMapModder <pemapmodder1970@gmail.com>
2014-02-22 12:50:48 +0800

    event

--- src/droidPluginGen/pluginGen/generator/Generator.java
+++ src/droidPluginGen/pluginGen/generator/Generator.java
@@ -2,13 +2,26 @@ package droidPluginGen.pluginGen.generator;
 
 import java.io.*;
 
-public class Generator {
-public Generator(){
-
-}
-@Override public String toString(){
-
-}
-public void outWrite(OutputStreamWriter osw)throws IOException{
-}
+public class Generator extends Object{
+	public Event[] events;
+	public Generator(){
+		
+	}
+	public boolean addEvent(String event, Function fx){
+		return addEvent(new Event(event, fx));
+	}
+	public boolean addEvent(Event event){
+		for(int i=0; i<events.length; i++){
+			if(events[i].equals(event)){
+				return false;
+		}
+		events[events.length]=event;
+		return true;
+	}
+	@Override public String toString(){
+		
+	}
+	public void outWrite (OutputStreamWriter osw) throws IOException{
+		
+	}
 }
