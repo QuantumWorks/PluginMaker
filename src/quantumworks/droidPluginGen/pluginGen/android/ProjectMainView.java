794833b21bda1d926c8c006eb9f52b45a4de8340
PEMapModder <pemapmodder1970@gmail.com>
2014-02-24 14:00:19 +0800

    Untitled commit

--- src/quantumworks/droidPluginGen/pluginGen/android/ProjectMainView.java
+++ src/quantumworks/droidPluginGen/pluginGen/android/ProjectMainView.java
@@ -14,7 +14,6 @@ public class ProjectMainView extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.project_main_view, menu);
 		return true;
 	}
